591e666dc0341e5a7b3356612896aa50083fd231
Adam Radocz <adam.radocz@gmail.com>
2020-05-03 12:49:35 +0200

    Docs: Fix spelling. Thanks to @davidshq (Dave Mackey): https://github.com/DevinVinson/WordPress-Plugin-Boilerplate/pull/521

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -29,7 +29,7 @@
 if (!defined('ABSPATH')) exit;
 
 /**
- * Currently plugin version.
+ * Current plugin version.
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
@@ -65,8 +65,8 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
 /**
  * Begins execution of the plugin.
  *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
+ * Since everything within the plugin is registered via hooks
+ * kicking off the plugin from this point in the file does
  * not affect the page life cycle.
  *
  * @since    1.0.0
