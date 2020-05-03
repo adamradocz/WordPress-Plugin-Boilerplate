9baa0f5c7d05c89ad4f688702d7157c762bcbaf8
Adam Radocz <adam.radocz@gmail.com>
2020-05-03 12:33:12 +0200

    Move the i18n to hooks. Fix some errors.

--- plugin-name/includes/class-plugin-name.php
+++ plugin-name/includes/class-plugin-name.php
@@ -64,7 +64,6 @@ class Plugin_Name {
 		$this->plugin_name = 'plugin-name';
 
 		$this->load_dependencies();
-		$this->set_locale();
 	}
 
 	/**
@@ -101,20 +100,6 @@ class Plugin_Name {
 	}
 
 	/**
-	 * Define the locale for this plugin for internationalization.
-	 *
-	 * Uses the Plugin_Name_i18n class in order to set the domain and to register the hook
-	 * with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function set_locale() {
-
-		$plugin_i18n = new Plugin_Name_i18n();
-	}
-
-	/**
 	 * Register all the hooks of the plugin.
 	 *
 	 * @since    1.0.0
@@ -127,7 +112,7 @@ class Plugin_Name {
 		 */
 		if (is_admin())
 		{
-			$plugin_admin = new Plugin_Name_Admin( $this->get_plugin_name(), $this->get_version() );
+			$plugin_admin = new Plugin_Name_Admin( $this->plugin_name, $this->version );
 			add_action( 'admin_enqueue_scripts', array( $plugin_admin, 'enqueue_styles' ) );
 			add_action( 'admin_enqueue_scripts', array( $plugin_admin, 'enqueue_scripts' ) );
 		}
@@ -136,7 +121,7 @@ class Plugin_Name {
 		 */
 		else
 		{
-			$plugin_public = new Plugin_Name_Public( $this->get_plugin_name(), $this->get_version() );
+			$plugin_public = new Plugin_Name_Public( $this->plugin_name, $this->version );
 			add_action( 'wp_enqueue_scripts', array( $plugin_public, 'enqueue_styles' ) );
 			add_action( 'wp_enqueue_scripts', array( $plugin_public, 'enqueue_scripts' ) );			 
 		}
@@ -144,6 +129,9 @@ class Plugin_Name {
 		/*
 		 * Includes hooks - Register all of the hooks related both to the admin area and to the public-facing functionality of the plugin.
 		 */
+		 
+		 // Set the domain for this plugin for internationalization.
+		 $plugin_i18n = new Plugin_Name_i18n();
 		 add_action( 'plugins_loaded', array( $plugin_i18n, 'load_plugin_textdomain' ) );		
 	}
 
